7f7f0eeb3a23bab63a24c950388b730d8a4b4e42
akshay-karad <akshaykarad24@gmail.com>
2026-09-06 12:03:02 +0530

    Create Recursive Fibonacci Program.py

---
+++ Recursive Fibonacci Program.py
@@ -0,0 +1,12 @@
+# Recursive Fibonacci Program
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+terms = int(input("Enter the number of terms: "))
+
+print("Fibonacci Series:")
+for i in range(terms):
+    print(fibonacci(i))
